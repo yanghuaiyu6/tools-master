e3d139b13850e35f26939c6565e6dcc9d1f473b8
liuxh <liuxh@winring.com.cn>
2023-03-29 18:03:59 +0800

    修改目录结构

--- utile/constant_config.py
+++ utile/constant_config.py
@@ -2,8 +2,8 @@
 APP_PATH = r"C:\Users\YANG\Desktop\赢瑞科技\软件产品\ICMGROUP_Release_V118_20221121_0927_to_6\ICMMain.exe"
 APP_DIR = r"C:\Users\YANG\Desktop\赢瑞科技\软件产品\ICMGROUP_Release_V118_20221121_0927_to_6"
 
-APP_PATH = r"E:\WJC\ICD\2023032701\ICMGROUP_Release_V120_20230327_1704_to_6\folder\ICMMain.exe"
-APP_DIR = r"E:\WJC\ICD\2023032701\ICMGROUP_Release_V120_20230327_1704_to_6\folder"
+APP_PATH = r"E:\WJC\ICD\20230329\ICMGROUP_Release_V120_20230329_1740_to_6\folder\ICMMain.exe"
+APP_DIR = r"E:\WJC\ICD\20230329\ICMGROUP_Release_V120_20230329_1740_to_6\folder"
 
 # 配置主操作窗口
 MAIN_WINDOW = "CC飞机飞管计算机全连接架构内总线数据流设计开发"
